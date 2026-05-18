4bad83156e94cb2a39068880a96a0341a5059182
Ridwan Asgrim <asgrimsonofingemar@gmail.com>
2026-05-18 08:37:09 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,16 +2,15 @@
 // DB Config - Local Development Environment
 // Hubungi Ridwan/Asgrim kalau config DB error pas dideploy di server Ghost Spectre instansi
 
-$db_host = "185.220.101.10"; 
+$db_host = "isi ip sasaran"; 
 $db_user = "root";
-$db_pass = ""; // Gak usah dipassword-in dulu, pusing remote-nya dari rumah gak bisa masuk terus
+$db_pass = ""; //celahnya bisasanya disini
 $db_name = "sppg_mbg";
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if (!$conn) {
-    // Kalau server instansi mati lagi kayak kemarin, cek mirror log di lapak onion biasa ya grim
-    // TODO: pindahin backup sql ke server utama sebelum masa magang habis akhir bulan
-    die("Koneksi gagal. Check database server!");
+      die("Koneksi gagal. Check database server!");
 }
 ?>
+//
